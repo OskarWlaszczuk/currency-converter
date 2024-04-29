c398eafd909a74abf131d5b4bc6a9714d76ad99d
Oskar Wlaszczuk <oskarwlaszczuk@gmail.com>
2024-04-29 20:37:00 +0200

    Converted js to the functions

--- js/script.js
+++ js/script.js
@@ -1,58 +1,82 @@
-let formElement = document.querySelector(".js-form");
-let inputFieldElement = document.querySelector(".js-form__inputField");
-let selectElement = document.querySelector(".js-form__select");
-let resualts = document.querySelector(".js-form__resualts");
+{
 
-let usdOption = document.querySelector(".js-form__usd").value;
-let gbpOption = document.querySelector(".js-form__gbp").value;
-let chfOption = document.querySelector(".js-form__chf").value;
-let eurOption = document.querySelector(".js-form__eur").value;
+    const displayResualt = (pln, usd, gbp, chf, eur) => {
+        const selectElement = document.querySelector(".js-form__select");
+        const currency = selectElement.value;
+        const resualts = document.querySelector(".js-form__resualts");
 
-formElement.addEventListener("input", () => {
-    let pln = inputFieldElement.value;
-    let currency = selectElement.value;
+        const usdOption = document.querySelector(".js-form__usd").value;
+        const gbpOption = document.querySelector(".js-form__gbp").value;
+        const chfOption = document.querySelector(".js-form__chf").value;
+        const eurOption = document.querySelector(".js-form__eur").value;
 
-    let usd = pln * 0.24;
-    let gbp = pln * 0.20;
-    let chf = pln * 0.22;
-    let eur = pln * 0.23;
 
-    switch (currency) {
-        case usdOption:
-            resualts.innerText = `${pln} zł = ${usd.toFixed(2)} $`;
-            selectElement.classList.toggle("form__select--usaTheme");
-            selectElement.classList.remove("form__select--engTheme");
-            selectElement.classList.remove("form__select--eurTheme");
-            selectElement.classList.remove("form__select--cheTheme");
-            break;
+        switch (currency) {
+            case usdOption:
+                resualts.innerHTML = `${pln} zł = <strong>${usd.toFixed(2)} $</strong`;
+                selectElement.classList.toggle("form__select--usaTheme");
+                selectElement.classList.remove("form__select--engTheme");
+                selectElement.classList.remove("form__select--eurTheme");
+                selectElement.classList.remove("form__select--cheTheme");
+                break;
 
-        case gbpOption:
-            resualts.innerText = `${pln} zł = ${gbp.toFixed(2)} GBP`;
-            selectElement.classList.toggle("form__select--engTheme");
-            selectElement.classList.remove("form__select--usaTheme");
-            selectElement.classList.remove("form__select--eurTheme");
-            selectElement.classList.remove("form__select--cheTheme");
-            break;
+            case gbpOption:
+                resualts.innerHTML = `${pln} zł = <strong>${gbp.toFixed(2)} £</strong>`;
+                selectElement.classList.toggle("form__select--engTheme");
+                selectElement.classList.remove("form__select--usaTheme");
+                selectElement.classList.remove("form__select--eurTheme");
+                selectElement.classList.remove("form__select--cheTheme");
+                break;
 
-        case chfOption:
-            resualts.innerText = `${pln} zł = ${chf.toFixed(2)} CHF`;
-            selectElement.classList.toggle("form__select--cheTheme");
-            selectElement.classList.remove("form__select--engTheme");
-            selectElement.classList.remove("form__select--eurTheme");
-            selectElement.classList.remove("form__select--usaTheme");
-            break;
+            case chfOption:
+                resualts.innerHTML = `${pln} zł = <strong>${chf.toFixed(2)} CHF</strong>`;
+                selectElement.classList.toggle("form__select--cheTheme");
+                selectElement.classList.remove("form__select--engTheme");
+                selectElement.classList.remove("form__select--eurTheme");
+                selectElement.classList.remove("form__select--usaTheme");
+                break;
 
-        case eurOption:
-            resualts.innerText = `${pln} zł = ${eur.toFixed(2)} € `;
-            selectElement.classList.toggle("form__select--eurTheme");
-            selectElement.classList.remove("form__select--usaTheme");
-            selectElement.classList.remove("form__select--engTheme");
-            selectElement.classList.remove("form__select--cheTheme");
-            break;
+            case eurOption:
+                resualts.innerHTML = `${pln} zł = <strong>${eur.toFixed(2)} €</strong> `;
+                selectElement.classList.toggle("form__select--eurTheme");
+                selectElement.classList.remove("form__select--usaTheme");
+                selectElement.classList.remove("form__select--engTheme");
+                selectElement.classList.remove("form__select--cheTheme");
+                break;
+        };
     };
-});
 
-formElement.addEventListener("reset", () => {
-    inputFieldElement.focus();
-    resualts.innerText = "";
-});
+    const calculateResualt = () => {
+        const inputElement = document.querySelector(".js-form__inputField");
+        const pln = inputElement.value;
+
+        const usd = pln * 0.24;
+        const gbp = pln * 0.20;
+        const chf = pln * 0.22;
+        const eur = pln * 0.23;
+
+        displayResualt(pln, usd, gbp, chf, eur);
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculateResualt();
+    };
+
+    const onFormReset = () => {
+        const inputElement = document.querySelector(".js-form__inputField");
+        const resualts = document.querySelector(".js-form__resualts");
+
+        inputElement.focus();
+        resualts.innerText = "";
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+        formElement.addEventListener("reset", onFormReset);
+    };
+
+    init();
+};
